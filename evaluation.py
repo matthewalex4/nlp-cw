633747a5564b95895d5521062be4f1ca132e6495
Matthew Alex <matalex412@gmail.com>
2026-02-21 09:21:36 +0000

    chore: add evaluation skeleton

---
+++ evaluation.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import sys
+import os
+import os.path
+from sklearn.metrics import precision_score, recall_score, f1_score, classification_report
+import numpy as np
+
+# as per the metadata file, input and output directories are the arguments
+[_, input_dir, output_dir] = sys.argv
+
+# unzipped submission data is always in the 'res' subdirectory
+# https://github.com/codalab/codalab-competitions/wiki/User_Building-a-Scoring-Program-for-a-Competition#directory-structure-for-submissions
+
+# define gold data path
+ref_dir = os.path.join(input_dir, 'ref')
+
+# define submission data path
+submission_dir = os.path.join(input_dir, 'res')
+files = os.listdir(submission_dir)
+
+# define path for scores file
+outf = open(os.path.join(output_dir,'scores.txt'),'w')
+
+# evaluating on task 1
+if 'task1.txt' in files:
+    task1_res = []
+    task1_gold = []
+    with open(os.path.join(submission_dir,'dev.txt')) as f:
+        for line in f:
+            task1_res.append(int(line.strip()))
+    with open(os.path.join(ref_dir,'dev.txt')) as f:
+        for line in f:
+            task1_gold.append(int(line.strip()))
+    # task 1 scores
+    t1p = precision_score(task1_gold, task1_res)
+    t1r = recall_score(task1_gold, task1_res)
+    t1f = f1_score(task1_gold, task1_res)
+    # task1
+    outf.write('task1_precision:'+str(t1p)+'\n')
+    outf.write('task1_recall:'+str(t1r)+'\n')
+    outf.write('task1_f1:'+str(t1f)+'\n')    
---
+++ load_data.py
@@ -0,0 +1,53 @@
+import os
+import pandas as pd
+
+class DontPatronizeMe:
+
+	def __init__(self, train_path, test_path):
+
+		self.train_path = train_path
+		self.test_path = test_path
+		self.train_task1_df = None
+		self.test_set_df = None
+
+	def load_task1(self):
+		"""
+		Load task 1 training set and convert the tags into binary labels. 
+		Paragraphs with original labels of 0 or 1 are considered to be negative examples of PCL and will have the label 0 = negative.
+		Paragraphs with original labels of 2, 3 or 4 are considered to be positive examples of PCL and will have the label 1 = positive.
+		It returns a pandas dataframe with paragraphs and labels.
+		"""
+		rows=[]
+		with open(os.path.join(self.train_path, 'dontpatronizeme_pcl.tsv')) as f:
+			for line in f.readlines()[4:]:
+				par_id=line.strip().split('\t')[0]
+				art_id = line.strip().split('\t')[1]
+				keyword=line.strip().split('\t')[2]
+				country=line.strip().split('\t')[3]
+				t=line.strip().split('\t')[4]#.lower()
+				l=line.strip().split('\t')[-1]
+				if l=='0' or l=='1':
+					lbin=0
+				else:
+					lbin=1
+				rows.append(
+					{'par_id':par_id,
+					'art_id':art_id,
+					'keyword':keyword,
+					'country':country,
+					'text':t, 
+					'label':lbin, 
+					'orig_label':l
+					}
+					)
+		df=pd.DataFrame(rows, columns=['par_id', 'art_id', 'keyword', 'country', 'text', 'label', 'orig_label']) 
+		self.train_task1_df = df
+
+	def load_test(self):
+		#self.test_df = [line.strip() for line in open(self.test_path)]
+		rows=[]
+		with open(self.test_path) as f:
+			for line in f:
+				t=line.strip().split('\t')
+				rows.append(t)
+		self.test_set_df = pd.DataFrame(rows, columns="par_id art_id keyword country text".split())
