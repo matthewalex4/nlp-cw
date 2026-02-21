75bddaeb3c2c613409843f9c690501f0307682c2
Matthew Alex <ma4723@ic.ac.uk>
2026-02-21 18:06:35 +0000

    feat: add test prediction setup

--- load_data.py
+++ load_data.py
@@ -44,10 +44,31 @@ class DontPatronizeMe:
 		self.train_task1_df = df
 
 	def load_test(self):
-		#self.test_df = [line.strip() for line in open(self.test_path)]
+		"""
+		Load test set and convert the tags into binary labels. 
+		"""
 		rows=[]
 		with open(self.test_path) as f:
-			for line in f:
-				t=line.strip().split('\t')
-				rows.append(t)
-		self.test_set_df = pd.DataFrame(rows, columns="par_id art_id keyword country text".split())
+			for line in f.readlines()[4:]:
+				par_id=line.strip().split('\t')[0]
+				art_id = line.strip().split('\t')[1]
+				keyword=line.strip().split('\t')[2]
+				country=line.strip().split('\t')[3]
+				t=line.strip().split('\t')[4]#.lower()
+				l=line.strip().split('\t')[-1]
+				if l=='0' or l=='1':
+					lbin=0
+				else:
+					lbin=1
+				rows.append(
+					{'par_id':par_id,
+					'art_id':art_id,
+					'keyword':keyword,
+					'country':country,
+					'text':t, 
+					'label':lbin, 
+					'orig_label':l
+					}
+					)
+		df=pd.DataFrame(rows, columns=['par_id', 'art_id', 'keyword', 'country', 'text', 'label', 'orig_label']) 
+		self.test_set_df = df
